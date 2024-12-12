456222161bde4f00faa661c0007bbbbe8acc0bef
emilien <emilien.lechat--maublanc@lamanu-student.fr>
2024-12-12 23:34:01 +0100

    made the player icon selector and submit button

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+let selectigPlayer=document.querySelector(".userIdPopup")
+let selectIcon=document.querySelector(".userIconSelected");
+let selectingIcon=document.querySelector(".userIconSelection");
+let creeper=document.querySelector(".creeper");
+let enderman=document.querySelector(".enderman");
+let enderDragon=document.querySelector(".enderDragon");
+let witherStorm=document.querySelector(".witherStorm");
+let herobrine=document.querySelector(".herobrine");
+let nickname=document.querySelector("#nickname").value;
+let submit=document.querySelector(".submitButton");
+let error=document.querySelector(".error");
+let overallPlayerIcon=document.querySelector(".playerIcon")
+
+selectIcon.addEventListener("click", function() {
+    selectingIcon.style.display="flex";
+});
+
+creeper.addEventListener("click", function() {
+    selectingIcon.style.display="none";
+    selectIcon.innerHTML='<img src="./assets/images/creeper.png" alt="minecraft creeper user icon">';
+    overallPlayerIcon.innerHTML='<img src="./assets/images/creeper.png" alt="minecraft creeper user icon">';
+});
+
+enderman.addEventListener("click", function() {
+    selectingIcon.style.display="none";
+    selectIcon.innerHTML='<img src="./assets/images/enderman.png" alt="minecraft enderman user icon">';
+    overallPlayerIcon.innerHTML='<img src="./assets/images/enderman.png" alt="minecraft enderman user icon">';
+});
+
+enderDragon.addEventListener("click", function() {
+    selectingIcon.style.display="none";
+    selectIcon.innerHTML='<img src="./assets/images/ender dragon.png" alt="minecraft ender dragon user icon">';
+    overallPlayerIcon.innerHTML='<img src="./assets/images/ender dragon.png" alt="minecraft ender dragon user icon">';
+});
+
+witherStorm.addEventListener("click", function() {
+    selectingIcon.style.display="none";
+    selectIcon.innerHTML='<img src="./assets/images/wither storm.webp" alt="minecraft wither storm user icon">';
+    overallPlayerIcon.innerHTML='<img src="./assets/images/wither storm.webp" alt="minecraft wither storm user icon">';
+});
+
+herobrine.addEventListener("click", function() {
+    selectingIcon.style.display="none";
+    selectIcon.innerHTML='<img src="./assets/images/herobrine.png" alt="minecraft herobrine user icon">';
+    overallPlayerIcon.innerHTML='<img src="./assets/images/herobrine.png" alt="minecraft herobrine user icon">';
+});
+
+submit.addEventListener("click", function(){
+    event.preventDefault();
+    selectigPlayer.style.display="none";
+    // if (nickname === "") {
+    //     console.log(nickname)
+    //     error.textContent = "Veuillez entrez un pseudo"
+    // }
+})
